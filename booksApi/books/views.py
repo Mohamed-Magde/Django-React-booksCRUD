87712619edff8411fb6d1f933998efa1f07c3ec8
Mohamed Magde Mohamed <mohamedmagdemohamed@MacBook-Pro.local>
2020-08-02 09:55:52 +0300

    first commit

---
+++ booksApi/books/views.py
@@ -0,0 +1,18 @@
+
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(generics.ListCreateAPIView):
+
+    queryset = Book.objects.all()
+
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Book.objects.all()
+
+    serializer_class = BookSerializer
